6522cb279c1cb91acb8f7ae89f97f0ccffeac248
isnanihidayati <37054272+isnanihidayati@users.noreply.github.com>
2018-10-16 08:51:07 +0700

    Add files via upload

---
+++ Praktikum-4/CaseIf_Else.java
@@ -0,0 +1,19 @@
+public class CaseIfElse {
+    
+    public static void main (String[][] args){
+        int stok = 47;
+        int jmlambil = 5;
+
+        if(jmlambil == stok){
+            System.out.println("Stok barang tidak mencukupi");
+        }else{
+            System.out.println("Data sudah sesuai");
+        }
+    }
+}
+
+
+// koreksi
+    /* terdapat double kurung siku [][] , harusnya 1 kurung siku [] saja
+        nama file dan nama class tidak sama, harusnya disamakan
+    */ 
