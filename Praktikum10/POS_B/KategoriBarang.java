a9f8a17a5b492c8647ec5c9aa5439441ae5c0853
isnanihidayati <37054272+isnanihidayati@users.noreply.github.com>
2019-01-08 17:04:40 +0700

    Add files via upload

---
+++ Praktikum10/POS_B/KategoriBarang.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum10.POS_B;
+
+/**
+ *
+ * @author CQ42
+ */
+public class KategoriBarang {
+    String kd_kategori;
+    String kategori_barang;
+    
+    private void getKategoriBarang(){
+        
+    }
+}
